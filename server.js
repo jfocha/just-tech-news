101d95804eb25e10999a70a5f1eeab4b0ca4f8e9
jfocha <jfocha@gmail.com>
2021-08-07 23:52:21 -0700

    used partials and helpers

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
